3f57566e3f430dde02c999f2445c7dc0137872b8
amir <amir-git@DESKTOP>
2024-12-24 23:30:44 +0330

    Add Migration

---
+++ Data/HotelListingDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+    }
+}
