1947837b563188a8d5466792f4b9fe67302c7ff0
amir <amir-git@DESKTOP>
2024-12-29 22:53:05 +0330

    Refactor Controller

--- Data/Country.cs
+++ Data/Country.cs
@@ -5,6 +5,9 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string ShortName { get; set; }
+
+
+
         public virtual IList<Hotel> Hotels { get; set;}
     }
 }
