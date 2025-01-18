0838c7f924802b4a3a54a1afdfa6e6015e7e4908
amir <amir-git@DESKTOP>
2025-01-18 19:15:23 +0330

    Implement Repository Pattern and AutoMapper

--- Contracts/ICountriesRepository.cs
+++ Contracts/ICountriesRepository.cs
@@ -4,5 +4,6 @@ namespace HotelListingAPI.Contracts
 {
     public interface ICountriesRepository : IGenericRepository<Country>
     {
+        Task<Country> GetDetails(int id);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 using HotelListingAPI.Configurations;
+using HotelListingAPI.Contracts;
 using HotelListingAPI.Data;
+using HotelListingAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -30,6 +32,12 @@ builder.Services.AddCors(options =>
 builder.Host.UseSerilog((context, configuration) => configuration.WriteTo.Console().ReadFrom.Configuration(context.Configuration));
 
 builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<ICountriesRepository , CountriesRepository>();
+
+
 
 var app = builder.Build();
 
